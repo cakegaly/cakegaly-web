8af05266fd1991aa6284abc47207cc2fb02f3dce
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-02-12 21:28:59 +0900

    feat: update site-footer & icons

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -7,38 +7,11 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
     <footer className={cn(className)}>
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Icons.logo />
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by{' '}
-            <a
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              cakegaly
-            </a>
-            . Hosted on{' '}
-            <a
-              href="https://vercel.com"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Vercel
-            </a>
-            . The source code is available on{' '}
-            <a
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              GitHub
-            </a>
-            .
+            &copy; {`${new Date().getFullYear()} ${siteConfig.copyRight}`}
           </p>
         </div>
+        <Icons.gitHub className="size-4" />
         {/* <ModeToggle /> */}
       </div>
     </footer>
