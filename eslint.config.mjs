ed01fc14562feab0ce32343f551258a4506f9d9c
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-02-22 22:24:23 +0900

    chore: (wip) add eslint-plugin-import & eslint-plugin-unused-imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,11 @@
 
 import eslintPluginNext from '@next/eslint-plugin-next';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import * as eslintPluginImport from 'eslint-plugin-import';
 import eslintPluginReact from 'eslint-plugin-react';
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
 import tailwindcss from 'eslint-plugin-tailwindcss';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
 import tsEslint from 'typescript-eslint';
 
 export default [
@@ -52,5 +54,38 @@ export default [
     },
   },
   tailwindcss,
+  {
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'always',
+          pathGroups: [
+            {
+              pattern: 'src/components/**',
+              group: 'internal',
+              position: 'before',
+            },
+            { pattern: 'src/lib/**', group: 'internal', position: 'before' },
+          ],
+        },
+      ],
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+    },
+  },
+  {
+    plugins: {
+      'unused-imports': eslintPluginUnusedImports,
+    },
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
   eslintConfigPrettier,
 ];
