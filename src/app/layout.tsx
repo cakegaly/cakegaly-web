e4af7fb5da1e2ce694992a0c4b821384678c8bb2
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-02-22 18:36:17 +0900

    refactor: remove sonner from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { SiteFooter } from '@/components/site-footer';
 import { SiteHeader } from '@/components/site-header';
-import { Toaster } from '@/components/ui/sonner';
 import { siteConfig } from '@/config/site';
 import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
@@ -67,7 +66,7 @@ export default function RootLayout({
           <main className="flex-1">{children}</main>
           <SiteFooter />
         </div>
-        <Toaster className="bg-primary" />
+        {/* <Toaster className="bg-primary" /> */}
         {/* </ThemeProvider> */}
       </body>
     </html>
