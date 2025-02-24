d44fcac3895855b8ec761d0fc943ca64ebc628e2
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-02-24 12:35:17 +0900

    feat: update mdx contents management

---
+++ src/lib/mdx.ts
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+
+type Frontmatter = {
+  title: string;
+  date: string;
+  [key: string]: unknown; // optional
+};
+
+type MDXData = {
+  metadata: Frontmatter;
+  slug: string;
+  content: string;
+};
+
+// dir name
+// path.join(process.cwd(), 'src/content/mdx-post');
+
+const getMDXFiles = (dir: string): string[] =>
+  fs.readdirSync(dir).filter((file) => path.extname(file) === '.mdx');
+
+const readMDXFile = async (filePath: string): Promise<MDXData> => {
+  const rawContent = await fs.promises.readFile(filePath, 'utf-8'); // 非同期に変更
+  const { metadata, content } = parseFrontmatter(rawContent);
+  const slug = path.basename(filePath, path.extname(filePath));
+
+  return { metadata, slug, content };
+};
+
+const getMDXData = async (dir: string): Promise<MDXData[]> => {
+  const mdxFiles = getMDXFiles(dir);
+  return Promise.all(mdxFiles.map((file) => readMDXFile(path.join(dir, file))));
+};
+
+const parseFrontmatter = (
+  rawContent: string
+): { metadata: Frontmatter; content: string } => {
+  const { data, content } = matter(rawContent);
+
+  return {
+    metadata: {
+      title: typeof data.title === 'string' ? data.title : 'Untitled',
+      date:
+        typeof data.date === 'string' ? data.date : new Date().toISOString(),
+      ...data,
+    },
+    content,
+  };
+};
+
+export const getMDXPosts = async (): Promise<MDXData[]> =>
+  getMDXData(path.join(process.cwd(), 'src/content/mdx-post'));
