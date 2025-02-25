4ad3b612487d25013b719df7e811154298057c12
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-02-26 03:11:27 +0900

    feat: update color theme for syntax highlight

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -131,7 +131,11 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+  plugins: [
+    require('tailwindcss-animate'),
+    require('@tailwindcss/typography'),
+    require('tailwind-scrollbar'),
+  ],
 } satisfies Config;
 
 export default config;
