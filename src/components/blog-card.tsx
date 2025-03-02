a30c50599a6a0b0141a392084c16ce1cbf785ea7
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-03-02 14:18:25 +0900

    feat: (wip) updat blog card

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -1,84 +1,33 @@
-import type { BlogPost } from '@/lib/mdx';
 import Link from 'next/link';
 
-import BlurImage from '@/components/blur-image';
-import { blurPlaceHolder, cn, formatDate } from '@/lib/utils';
-
-import { Author } from '@/components/author';
-import { siteConfig } from '@/config/site';
+import { Icons } from '@/components/icons';
+import { BlogPost } from '@/lib/mdx';
+import { formatDate } from '@/lib/utils';
 
 interface BlogCardProps {
-  data: BlogPost & {
-    blurDataURL: string;
-  };
-  priority?: boolean;
-  variant?: 'vertical' | 'horizontal';
+  data: BlogPost;
 }
 
-export function BlogCard({
-  data,
-  priority,
-  variant = 'vertical',
-}: BlogCardProps) {
-  const thumbnail = siteConfig.ogImage;
+export function BlogCard({ data }: BlogCardProps) {
+  const { metadata } = data;
+  // const EyecatchIcon = Icons[metadata.eyecatch ?? 'fileText'] ?? Icons.fileText;
+  const EyecatchIcon = Icons.calendar;
 
   return (
-    <article
-      className={cn(
-        'group relative',
-        variant === 'horizontal'
-          ? 'grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-6'
-          : 'flex flex-col space-y-2'
-      )}
-    >
-      {thumbnail && (
-        <div className="w-full overflow-hidden rounded-xl border">
-          <BlurImage
-            alt={data.metadata.title}
-            blurDataURL={data.blurDataURL ?? blurPlaceHolder}
-            className={cn(
-              'size-full object-cover object-center',
-              variant === 'horizontal' ? 'lg:h-72' : null
-            )}
-            width={800}
-            height={400}
-            priority={priority}
-            placeholder="blur"
-            src={thumbnail}
-            sizes="(max-width: 768px) 750px, 600px"
-          />
-        </div>
-      )}
-      <div
-        className={cn(
-          'flex flex-1 flex-col',
-          variant === 'horizontal' ? 'justify-center' : 'justify-between'
-        )}
-      >
-        <div className="w-full">
-          <h2 className="my-1.5 line-clamp-2 text-2xl font-bold">
-            {data.metadata.title}
-          </h2>
-          {data.metadata.description && (
-            <p className="line-clamp-2 text-muted-foreground">
-              {data.metadata.description}
-            </p>
-          )}
-        </div>
-        <div className="mt-4 flex items-center space-x-3">
-          <div className="flex items-center -space-x-2">
-            <Author imageOnly />
-          </div>
-
-          {data.metadata.date && (
-            <p className="text-sm text-muted-foreground">
-              {formatDate(data.metadata.date)}
-            </p>
-          )}
+    <article className="group relative flex gap-4">
+      <div className="inline-flex size-24 items-center justify-center rounded-sm">
+        <EyecatchIcon className="size-12" />
+        <span className="sr-only">{'expression icon'}</span>
+      </div>
+      <div className="flex-1">
+        <div className="font-bold group-hover:underline">{metadata.title}</div>
+        <div className="mt-2 inline-flex items-center gap-2 text-xs">
+          <Icons.calendar className="size-4" />
+          <time dateTime={metadata.date}>{formatDate(metadata.date)}</time>
         </div>
       </div>
-      <Link href={`blog/${data.slug}`} className="absolute inset-0">
-        <span className="sr-only">View Article</span>
+      <Link href={`/blog/${data.slug}`} className="absolute inset-0">
+        <span className="sr-only">{'記事を読む'}</span>
       </Link>
     </article>
   );
