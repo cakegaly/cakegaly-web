53ad2468b44ee2280dc98d09a032e457830cda1d
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-03-04 23:01:46 +0900

    refactor: remove comments from sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,3 @@
-// import { MetadataRoute } from 'next';
-
-// /**
-//  * @see https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap#generating-multiple-sitemaps
-//  */
-// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-//   const sitemapEntries: MetadataRoute.Sitemap = [];
-
-//   // top page
-//   sitemapEntries.push({
-//     url: `${process.env.NEXT_PUBLIC_APP_URL}`,
-//   });
-
-//   // blog page
-//   // const posts = await getPosts();
-//   // ...
-
-//   return sitemapEntries;
-// }
 import { siteConfig } from '@/config/site';
 import { getBlogPosts } from '@/lib/mdx';
 import { MetadataRoute } from 'next';
