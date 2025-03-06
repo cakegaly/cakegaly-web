f71ef8dbce1dd50a7df22555ef99620fee0a1766
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-03-06 22:12:53 +0900

    feat: update tag page

--- src/app/tag/[slug]/page.tsx
+++ src/app/tag/[slug]/page.tsx
@@ -1,44 +1,46 @@
 import { BlogCard } from '@/components/content/blog-card';
 import { tags } from '@/config/blog';
+import { siteConfig } from '@/config/site';
 import { getBlogPostsByTagSlug } from '@/lib/mdx';
+import { absoluteUrl } from '@/lib/utils';
 
 interface TagPageProps {
   params: Promise<{ slug: string }>;
 }
 
 // TODO: set tag description & customize metadata of this page
-// export async function generateMetadata({ params }: TagPageProps) {
-//   const { slug } = await params;
-//   const tag = tags.slug;
-//   if (!tag) {
-//     return {};
-//   }
+export async function generateMetadata({ params }: TagPageProps) {
+  const { slug } = await params;
+  const tag = tags.slug;
+  if (!tag) {
+    return {};
+  }
 
-//   return {
-//     title: `「${tag.name}」タグの記事一覧`,
-//     description: '',
-//     openGraph: {
-//       title: `「${tag.name}」タグの記事一覧`,
-//       description: '',
-//       type: 'article',
-//       url: absoluteUrl(`/tag/${slug}`),
-//       images: [
-//         {
-//           url: siteConfig.ogImage,
-//           width: 1200,
-//           height: 630,
-//           alt: `「${tag.name}」タグの記事一覧`,
-//         },
-//       ],
-//     },
-//     twitter: {
-//       card: 'summary_large_image',
-//       title: `「${tag.name}」タグの記事一覧`,
-//       description: '',
-//       images: [siteConfig.ogImage],
-//     },
-//   };
-// }
+  return {
+    title: `「${tag.name}」タグの記事一覧`,
+    description: `「${tag.name}」タグの記事一覧を表示しています。`,
+    openGraph: {
+      title: `「${tag.name}」タグの記事一覧`,
+      description: `「${tag.name}」タグの記事一覧を表示しています。`,
+      type: 'article',
+      url: absoluteUrl(`/tag/${slug}`),
+      images: [
+        {
+          url: siteConfig.ogImage,
+          width: 1200,
+          height: 630,
+          alt: `「${tag.name}」タグの記事一覧`,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `「${tag.name}」タグの記事一覧`,
+      description: `「${tag.name}」タグの記事一覧を表示しています。`,
+      images: [siteConfig.ogImage],
+    },
+  };
+}
 
 export async function generateStaticParams() {
   return Object.keys(tags).map((slug) => ({
