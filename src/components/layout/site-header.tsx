e45ea0b6aecad62fb81359cd8459114d531e614e
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-03-10 21:39:03 +0900

    feat: update site-header

--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -7,16 +7,18 @@ export function SiteHeader() {
   return (
     <header className="container sticky top-0 z-40 border-b bg-background/50 backdrop-blur-md">
       <div className="mx-auto flex h-16 max-w-screen-md items-center justify-between">
-        <Link href="/" className="rounded-sm p-2">
+        <Link
+          href="/"
+          className="flex items-center gap-2 rounded-md p-2 hover:bg-accent/20"
+        >
           <Image
             src="/images/cakegaly.webp"
-            alt="logo"
-            width="40"
-            height="40"
-            title="logo"
-            className="rounded-full"
+            alt="Cakegaly logo"
+            width="32"
+            height="32"
+            className="rounded-full ring-1 ring-border"
           />
-          <span className="sr-only">Home</span>
+          <span className="font-mono text-sm">cakegaly.com</span>
         </Link>
         <ModeSwitch />
       </div>
