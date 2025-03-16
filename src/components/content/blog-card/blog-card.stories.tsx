0d2f766bc0db28cc62e36cbe68b13393ab22c229
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-03-17 03:48:17 +0900

    feat: update blog-card story

--- src/components/content/blog-card/blog-card.stories.tsx
+++ src/components/content/blog-card/blog-card.stories.tsx
@@ -12,19 +12,45 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const mockData = {
+const createMockData = (icon: keyof typeof TechIcons, title?: string) => ({
   metadata: {
-    title: 'サンプル記事タイトル',
+    title: title || `サンプル記事: ${icon}`,
     description: 'これはサンプル記事の説明文です。',
     date: '2024-04-01',
     tags: ['react', 'nextjs'],
-    icon: 'react' as keyof typeof TechIcons,
+    icon,
   },
-  slug: 'sample-post',
-};
+  slug: `${icon}-sample-post`,
+});
 
 export const Default: Story = {
   args: {
-    data: mockData,
+    data: createMockData('react'),
   },
+};
+
+export const Icon_ESLint: Story = { args: { data: createMockData('eslint') } };
+
+export const Icon_Jamstack: Story = {
+  args: { data: createMockData('jamstack') },
+};
+
+export const Icon_Nextjs: Story = { args: { data: createMockData('nextjs') } };
+
+export const Icon_React: Story = { args: { data: createMockData('react') } };
+
+export const Icon_TailwindCSS: Story = {
+  args: { data: createMockData('tailwindcss') },
+};
+
+export const Icon_TypeScript: Story = {
+  args: { data: createMockData('typescript') },
+};
+
+export const Icon_Vercel: Story = { args: { data: createMockData('vercel') } };
+
+export const Icon_VSCode: Story = { args: { data: createMockData('vscode') } };
+
+export const Icon_WordPress: Story = {
+  args: { data: createMockData('wordpress') },
 };
