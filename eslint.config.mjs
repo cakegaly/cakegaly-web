cba94f45001c60852989e740e24d7f40f81e6f68
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-12-06 19:52:46 +0900

    feat: eslint packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,79 +1,25 @@
-// @ts-check
-import eslintJs from '@eslint/js';
-import pluginNext from '@next/eslint-plugin-next';
-import configPrettier from 'eslint-config-prettier';
-import pluginReact from 'eslint-plugin-react';
-import pluginReactHooks from 'eslint-plugin-react-hooks';
-import pluginTypeScript from 'typescript-eslint';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-const config = [
-  // Base
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  globalIgnores([
+    'node_modules/**',
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
   {
-    ignores: [
-      '**/build/',
-      '**/bin/',
-      '**/dist/',
-      '**/obj/',
-      '**/out/',
-      '**/.next/',
-      '**/node_modules/',
-    ],
-  },
-
-  // TypeScript
-  {
-    name: 'eslint/recommended',
-    rules: eslintJs.configs.recommended.rules,
-  },
-  ...pluginTypeScript.configs.recommended,
-
-  // React
-  {
-    name: 'react/jsx-runtime',
-    plugins: {
-      react: pluginReact,
-    },
-    rules: pluginReact.configs['jsx-runtime'].rules,
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  },
-  {
-    name: 'react-hooks/recommended',
-    plugins: {
-      'react-hooks': pluginReactHooks,
-    },
-    rules: pluginReactHooks.configs.recommended.rules,
-  },
-
-  // Next.js
-  {
-    name: 'next/core-web-vitals',
-    plugins: {
-      '@next/next': pluginNext,
-    },
-    rules: {
-      ...pluginNext.configs.recommended.rules,
-      ...pluginNext.configs['core-web-vitals'].rules,
-    },
-  },
-
-  // Prettier
-  {
-    name: 'prettier/config',
-    ...configPrettier,
-  },
-
-  // Project custom rules
-  {
-    name: 'project-custom',
     rules: {
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
-];
+]);
 
-export default config;
+export default eslintConfig;
