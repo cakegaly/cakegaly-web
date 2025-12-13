2e54fb723d6078d38f7836a67ffbf9ec195d1215
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-12-13 14:53:09 +0900

    feat: root page

--- src/app/(app)/(root)/page.tsx
+++ src/app/(app)/(root)/page.tsx
@@ -23,35 +23,41 @@ export default async function IndexPage() {
       </div>
       <div className="container-wrapper">
         <div className="container py-6">
-          <div className="flex flex-col gap-2">
-            {allPosts.map((blog, i) => (
-              <div key={i} className="flex w-full justify-between">
-                <TextLink
-                  href={`/blog/${blog.slug}`}
-                  size="sm"
-                  className="line-clamp-1"
-                >
-                  {blog.metadata.title}
-                </TextLink>
-                <span className="text-on-muted shrink-0 text-xs">
-                  {formatDate(blog.metadata.date)}
-                </span>
-              </div>
-            ))}
+          <div className="flex flex-col gap-4">
+            <h2 className="text-lg font-bold">Latest Blog</h2>
+            <div className="flex flex-col gap-2">
+              {allPosts.map((blog, i) => (
+                <div key={i} className="flex w-full justify-between">
+                  <TextLink
+                    href={`/blog/${blog.slug}`}
+                    size="sm"
+                    className="line-clamp-1"
+                  >
+                    {blog.metadata.title}
+                  </TextLink>
+                  <span className="text-on-muted shrink-0 text-xs">
+                    {formatDate(blog.metadata.date)}
+                  </span>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
       <div className="container-wrapper">
         <div className="container py-6">
-          <div className="flex flex-wrap gap-2">
-            {siteConfig.navItems.map((tool, i) => (
-              <Button key={i} variant="ghost" asChild>
-                <Link href={tool.href}>
-                  <WrenchIcon />
-                  {tool.title}
-                </Link>
-              </Button>
-            ))}
+          <div className="flex flex-col gap-4">
+            <h2 className="text-lg font-bold">Tools</h2>
+            <div className="flex flex-wrap gap-2">
+              {siteConfig.navItems.map((tool, i) => (
+                <Button key={i} variant="ghost" size="sm" asChild>
+                  <Link href={tool.href}>
+                    <WrenchIcon />
+                    {tool.title}
+                  </Link>
+                </Button>
+              ))}
+            </div>
           </div>
         </div>
       </div>
