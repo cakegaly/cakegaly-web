6e927fde6585cbc24ad6b276f87b413ddf5d1209
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-12-28 17:51:20 +0900

    feat: site header

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,8 +4,8 @@ import { SiteHeader } from '@/components/layout/site-header';
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-canvas relative z-10 flex min-h-svh flex-col">
-      <SiteHeader />
-      <main className="flex flex-1 flex-col">{children}</main>
+      <SiteHeader className="sticky top-4" />
+      <main className="flex flex-1 flex-col pt-4">{children}</main>
       <SiteFooter />
     </div>
   );
