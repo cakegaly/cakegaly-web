65f5cefc0352ed79ddf4107e009946241fe78838
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-12-31 17:31:06 +0900

    feat: blog heading

--- src/components/content/mdx-components.tsx
+++ src/components/content/mdx-components.tsx
@@ -30,6 +30,7 @@ export const mdxComponents = {
     <h2
       className={cn(
         'border-accent/50 mt-12 scroll-m-20 border-b pb-2 text-xl font-medium tracking-tight first:mt-0',
+        "before:text-on-muted before:mr-1.5 before:text-xl before:content-['##']",
         className
       )}
       {...props}
@@ -39,6 +40,7 @@ export const mdxComponents = {
     <h3
       className={cn(
         'mt-8 scroll-m-20 text-lg font-medium tracking-tight',
+        "before:text-on-muted before:mr-1.5 before:text-lg before:content-['###']",
         className
       )}
       {...props}
@@ -48,6 +50,7 @@ export const mdxComponents = {
     <h4
       className={cn(
         'mt-8 scroll-m-20 text-base font-medium tracking-tight',
+        "before:text-on-muted before:mr-1.5 before:text-base before:content-['####']",
         className
       )}
       {...props}
